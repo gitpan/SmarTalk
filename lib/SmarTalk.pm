a4820823a42c3547adaa4620c7926a097a7ce35f
Cladi Di Domenico <kingitalia@gmail.com>
2013-01-30 14:03:20 +0000

    Import of CLADI/SmarTalk_v09 from CPAN.
    gitpan-cpan-distribution: SmarTalk
    gitpan-cpan-version:      v09
    gitpan-cpan-path:         CLADI/SmarTalk_v09.tar.gz
    gitpan-cpan-author:       CLADI
    gitpan-cpan-maturity:     released

--- lib/SmarTalk.pm
+++ lib/SmarTalk.pm
@@ -12,7 +12,7 @@ our @ISA = qw(Exporter);
 our %EXPORT_TAGS = ('all' => [ qw(new setServer serverUp setClient clientUp)]);
 our @EXPORT_OK = (@{$EXPORT_TAGS{'all'}});
 our @EXPORT = qw();
-our $VERSION = '0.07';
+our $VERSION = '0.09';
         
 #declaration
 my ($tunnel,$settingServer,$settingClient,$pcremoto,
